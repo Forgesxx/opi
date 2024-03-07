5a780a240192714aaf20fb3580d31c770891153d
ROG\forge <Forgesx236@gmail.com>
2024-03-07 11:49:27 +0200

    finally relocated proj

--- opi/src/App.js
+++ opi/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AuthComponent from './components/AuthComponent/AuthPage.jsx';
+import Header from './components/header/header';
+import { AuthProvider } from './components/AuthComponent/AuthComponent.jsx';
+import AllMovies from './components/movieInfo/allMovies.jsx';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/mainScreenMovies" element={<AllMovies/>} />
+            <Route path="/" element={<AuthComponent />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
